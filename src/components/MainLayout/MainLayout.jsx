100941e8e140e221001bdef2fc129e1919652546
kawsarhossen1 <kawsarhossen66@gmail.com>
2025-07-12 15:49:21 +0600

    Initial commit: navbar and layout setup

--- src/components/MainLayout/MainLayout.jsx
+++ src/components/MainLayout/MainLayout.jsx
@@ -1,6 +1,8 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Navbar/Navbar";
 import Footer from "../Footer/Footer";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const MainLayout = () => {
   return (
@@ -10,6 +12,7 @@ const MainLayout = () => {
         <Outlet></Outlet>
       </div>
       <Footer></Footer>
+      <ToastContainer position="top-center" autoClose={3000} />
     </div>
   );
 };
